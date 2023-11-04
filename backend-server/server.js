1a41bb34f92256da6fb0db16c19c5b00100c777b
sinh <le.sinhhh2507@gmail.com>
2023-11-04 23:19:21 +0100

    feat: formatted code style

---
+++ backend-server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from the backend server!');
+});
+
+// Define additional routes and API endpoints as needed
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
